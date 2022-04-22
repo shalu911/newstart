862267c5af66b6149c1f54b7ed625753175ed110
shalu devda <94682838+shalu911@users.noreply.github.com>
2022-04-22 19:47:01 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,13 @@
+x=5
+y="shalu"
+print(x)
+print(y)
+print(type(x)) #type is used for see the type of the data type
+# print(x+y)//we can not concatinate int/numerical data type whith string
+#string interpolation
+w1="shanu"
+w2="tanushree"
+msg ='my name is {} but real name is{}ok.'
+print(msg.format(w1,w2))
+s=range(23)
+print(s)
